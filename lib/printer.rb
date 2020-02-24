8fdc95332e0fcab3cdb5272af854b78e15270362
Umberto Valente <uvalente89@gmail.com>
2020-02-24 15:56:25 +0000

    feat: add header method to Printer, print now return a transaction string

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Printer
-  def print
-    puts 'date || credit || debit || balance'
+  def header
+    'date || credit || debit || balance'
+  end
+
+  def print(transaction)
+    "#{transaction.date.strftime('%d/%m/%Y')} || #{transaction.credit} || #{transaction.debit} || #{transaction.balance}"
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -3,8 +3,19 @@
 require 'printer'
 
 describe Printer do
+  let(:deposit) { double(:transaction, date: Date.new(2020, 0o2, 24), credit: 1000, debit: nil, balance: 1000) }
+  let(:withdraw) { double(:transaction, date: Date.new(2020, 0o2, 24), credit: nil, debit: 500, balance: 2500) }
+
   it 'print a header' do
     header = "date || credit || debit || balance\n"
-    expect { subject.print }.to output(header).to_stdout
+    expect(subject.header).to match(/#{header}/)
+  end
+
+  it 'print a deposit transaction' do
+    expect(subject.print(deposit)).to eq '24/02/2020 || 1000 ||  || 1000'
+  end
+
+  it 'print a withdraw transaction' do
+    expect(subject.print(withdraw)).to eq '24/02/2020 ||  || 500 || 2500'
   end
 end
